d3e25ee80f8e7cf8db14281e33cac86b20bb64c2
leonardofranca <leonardo5155@gmail.com>
2023-06-01 16:21:36 -0300

    declarando um array

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //teclarando um array de notas
+        int[] notas = {8, 5, 10, 7};
+
+        //imprimindo as notas
+        System.out.printf("Nota: %d\n", notas[0]);
+        System.out.printf("Nota: %d\n", notas[1]);
+        System.out.printf("Nota: %d\n", notas[2]);
+        System.out.printf("Nota: %d\n", notas[3]);
+
+
+    }
+}
